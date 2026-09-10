a00612fdf718fa46650c7f5c65f53881bed9c37f
n2023ik <pandeynikhil429@gmail.com>
2026-09-10 11:12:25 +0530

    fix Vercel TanStack Start deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [tanstackStart(), react(), tailwindcss()],
+	plugins: [tanstackStart(), nitro(), react(), tailwindcss()],
 	resolve: {
 		tsconfigPaths: true,
 	},
